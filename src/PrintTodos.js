8c7776fe50cc0582d29181b6c69c5538791d09d6
unknown <Divyapancholi1995@gmail.com>
2023-04-19 09:00:43 -0500

    add delete feature and model

--- src/PrintTodos.js
+++ src/PrintTodos.js
@@ -1,19 +1,31 @@
-import {useContext} from "react";
+import {useContext,useState,useRef} from "react";
 import AddContext from "./AddContext";
+import { Modal } from "./Modal";
 const PrintTOdos = () => {
     let [todo,setTodo] = useContext(AddContext);
+    let [showModal,setShowModal] = useState(false);
+    let [string,setString] = useState("");
     return(
         <div>
                 <table>
                     <tbody>
-                    {todo.map((todo)=>
+                    {todo.map((element,index)=>
                     <tr>
-                        <td>{todo}</td>
-                        <td><button>Delete</button></td>
+                        <td id={index}>{element}</td>
+                        <td><button id={index} onClick={()=>{setShowModal(true);  setString(index)}}>Delete</button></td>
                     </tr>
                     )}
                     </tbody>
                 </table>
+                {showModal?(
+                <Modal>
+                    <h1>Are you sure?</h1>
+                    <h2>Remove "{todo[string]}"</h2>
+                    <button onClick={()=>{setShowModal(false); setTodo(todo.filter((item,id)=>id != string))}}> Yes </button>
+                    <button onClick={()=>setShowModal(false)}> No </button>
+                </Modal>
+                ):null}
+                
             
         </div>
     )
